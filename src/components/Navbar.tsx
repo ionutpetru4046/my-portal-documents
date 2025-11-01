1539497171289163dcd1952ee35960d8e3d474e9
ionutpetru4046 <ionut4046@yahoo.com>
2025-11-01 00:05:22 +0000

    fixing login and get started button on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -346,12 +346,12 @@ export default function Navbar() {
               ) : (
                 <div className="flex items-center gap-2">
                   <Link href="/login">
-                    <Button className="bg-slate-800 hover:bg-slate-700 text-slate-300 hover:text-white font-medium text-sm border border-slate-700 hover:border-slate-600 rounded-lg px-4 py-2 transition-all">
+                    <Button className="bg-slate-800 cursor-pointer hover:bg-slate-700 text-slate-300 hover:text-white font-medium text-sm border border-slate-700 hover:border-slate-600 rounded-lg px-4 py-2 transition-all">
                       Login
                     </Button>
                   </Link>
                   <Link href="/signup">
-                    <Button className="bg-linear-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 text-white font-medium text-sm rounded-lg px-4 py-2 shadow-lg hover:shadow-blue-500/20 transition-all">
+                    <Button className="bg-linear-to-r from-blue-600 to-purple-600 hover:from-blue-700 cursor-pointer hover:to-purple-700 text-white font-medium text-sm rounded-lg px-4 py-2 shadow-lg hover:shadow-blue-500/20 transition-all">
                       Get Started
                     </Button>
                   </Link>
