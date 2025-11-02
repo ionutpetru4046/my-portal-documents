bab8fad6cdba9528bd01bc35f5e32ed2160cb887
ionutpetru4046 <ionut4046@yahoo.com>
2025-11-02 00:11:57 +0000

    fixing next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // allow Supabase storage host as a remote image source
-    domains: ["qqchawvrepircjjrcmcl.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
